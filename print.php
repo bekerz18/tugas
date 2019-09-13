c8261f5a1f8981912a63a37d3edb1182e6b8a4d8
Bahtiar <zamkorong12@gmail.com>
2019-09-14 03:18:03 +0700

    menambahkan nganu

--- print.php
+++ print.php
@@ -1,8 +1,54 @@
-<?php
-echo "$_POST[nama]";
-echo "$_POST[nis]";
-echo "$_POST[tempat_lahir]";
-echo "$_POST[tanggal_lahir]";
-echo "$_POST[agama]";
-echo "$_POST[darah]";
-echo "$_POST[alamat_lengkap]";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Student Registration - SMA NEGERI 4 TASIKMALAYA</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="col-50 form-registrasi" style="background-color: #FAFAFA;height:300px;border-radius:5px;">
+        <div class="fill-id">
+            <div id="header col-100">
+                <div class="logo col-20" style="float:left;">
+                    <img src="logopatas.png" style="width:60%;"alt="">
+                </div>
+                <div class="kop col-80" style="float:left;">
+                    <span class="sekolah">SMA NEGERI 4 Tasikmalaya</span>
+                    <span class="alamat">Jl. LetKol RE DJaelani, Cilembang, Kec. Cihideung, Tasikmalaya</span>
+                </div>
+            </div>
+            <div id="data">
+                <table>
+                    <tr>
+                        <td>NIS</td>
+                        <td>: <?php echo "$_POST[nis]";?></td>
+                        <td rowspan="5"><img src="logopatas.png" style="width:20%;" alt=""></td>
+                    </tr>
+                    <tr>
+                        <td>Nama Lengkap</td>
+                        <td>: <?php echo "$_POST[nama]";?></td>
+                    </tr>
+                    <tr>
+                        <td>jenis Kelamin</td>
+                        <td>: <?php echo "$_POST[kelamin]";?></td>
+                    </tr>
+                    <tr>
+                        <td>Tempat, Tanggal Lahir</td>
+                        <td>: <?php echo "$_POST[tempat_lahir], $_POST[tanggal_lahir]";?></td>
+                    </tr>
+                    <tr>
+                        <td>Alamat</td>
+                        <td>: <?php echo "$_POST[alamat_lengkap]";?></td>
+                    </tr>
+                    <tr>
+                        <td>Agama</td>
+                        <td>: <?php echo "$_POST[agama]";?></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+    </body>
+</html>
--- register.php
+++ register.php
@@ -5,38 +5,62 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Student Registration - SMA NEGERI 4 TASIKMALAYA</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <form method="POST" action="print.php">
-        <label>Nama Lengkap</label>
-        <input type="text" name="nama" required autofocus/>
-        <label>Nomor Identitas Siswa</label>
-        <input type="text" name="nis" required/>
-        <label>Tempat, Tanggal Lahir</label>
-        <input type="text" name="tempat_lahir" required/>
-        <input type="date" name="tanggal_lahir" required/>
-        <label>Agama</label>
-        <select name="agama" required>
-            <option value="Silahkan Pilih">Silahkan Pilih</option>
-            <option value="Islam">Islam</option>
-            <option value="Kristen Protestan">Kristen Protestan</option>
-            <option value="Kristen Katolik">Kristen Katolik</option>
-            <option value="Hindu">Hindu</option>
-            <option value="Buddha">Buddha</option>
-            <option value="Konghucu">Konghucu</option> 
-        </select>
-        <label>Gol Darah</label>
-        <select name="darah" required>
-            <option value="Silahkan Pilih">Silahkan Pilih</option>
-            <option value="A">A</option>
-            <option value="B">B</option>
-            <option value="AB">AB</option>
-            <option value="O">O</option>
-            <option value="-">-</option>
-        </select>
-        <label>Alamat</label>
-        <textarea name="alamat_lengkap" required></textarea>
-        <input name="submit" type="submit"/>
-    </form>
+        <form class="form-registrasi" method="post" action="print.php">
+            <div class="form-kanan col-50">
+                <div class="fill">
+                    <img src="logopatas.png" alt="" style="width:30%;height:auto;margin:0 auto;display:block"/>          
+                    <h1 align="center">ID Card Generator</h1>
+                    <p>Silahkan masukan beberapa data dan klik register untuk mendapatkan ID Card.</p>
+                </div>
+            </div>
+            <div class="form-kiri col-50">
+                <div class="fill" style="margin-left:10px;">
+                    <!-- <div class="chunk">
+                        <label>Nama Lengkap</label> -->
+                        <div class="chunk">
+                            <input type="text" class="kontrol"placeholder="Nomor Identitas Siswa" name="nis" required/>
+                        </div>
+                        <div class="chunk">
+                            <input type="text" class="kontrol" placeholder="Nama Lengkap" name="nama" required autofocus/>
+                            <input type="radio" name="kelamin" value="Laki-Laki">Laki-Laki</input>
+                            <input type="radio" name="kelamin" value="Perempuan">Perempuan</input>
+                        </div>
+                    <!-- </div>
+                    <div class="chunk">
+                        <label>Nomor Identitas Siswa</label> -->
+                        
+                    <!-- </div>
+                    <div class="chunk">
+                        <label>Tempat, Tanggal Lahir</label> -->
+                        <div class="chunk">
+                            <input type="text" class="kontrol"placeholder="Tempat Lahir" name="tempat_lahir" required/>
+                            <input type="date" class="kontrol" name="tanggal_lahir" required/>
+                        </div>
+                    <!-- </div> -->
+                    <!-- <div class="chunk">
+                        <label>Agama</label> -->
+                        <div class="chunk">
+                            <textarea class="kontrol"  placeholder="Alamat Lengkap" name="alamat_lengkap" required></textarea>
+                        </div>
+                        <div class="chunk">
+                            <select class="kontrol" name="agama" required>
+                                <option value="Silahkan Pilih">Silahkan Pilih Agama</option>
+                                <option value="Islam">Islam</option>
+                                <option value="Kristen Protestan">Kristen Protestan</option>
+                                <option value="Kristen Katolik">Kristen Katolik</option>
+                                <option value="Hindu">Hindu</option>
+                                <option value="Buddha">Buddha</option>
+                                <option value="Konghucu">Konghucu</option> 
+                            </select>
+                        </div>
+                </div>
+                <div class="chunk" style="margin-top:-42px;">
+                    <input class="kontrol col-100" name="submit" type="submit" value="Register"/>
+                </div>
+            </div>
+        </form>
 </body>
 </html>
