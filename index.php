fd449cf5a19c1c12f29f6000b6204da048a9ab76
Bahtiar <zamkorong12@gmail.com>
2019-09-23 21:06:29 +0700

    menambahkan datatables

--- index.php
+++ index.php
@@ -7,9 +7,8 @@
     <title>Student Registration - SMA NEGERI 4 TASIKMALAYA</title>
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" type="text/css" href="plugins/datatables/datatables.css"/>
-    
-
-    
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.5.6/css/buttons.dataTables.min.css">
 </head>
 <body>
         <form class="form-registrasi" method="post" action="print.php" enctype="multipart/form-data">
@@ -32,7 +31,7 @@
                         </div>
                         <div class="chunk">
                             <input type="text" class="kontrol"placeholder="Tempat Lahir" name="tempat_lahir" required/>
-                            <input type="date" class="kontrol" name="tanggal_lahir"  required/>
+                            <input type="date" class="kontrol" name="tanggal_lahir" required/>
                         </div>
                         <div class="chunk">
                             <textarea class="kontrol"  placeholder="Alamat Lengkap" name="alamat_lengkap" required></textarea>
@@ -56,37 +55,38 @@
             </div>
         </form>
         <div class="clear"></div>
-        <div class="data col-80">
+        <br><br>
+        <div class="data col-90">
             <div class="judul-data">
                 <span>Data</span>
             </div>
-            <table id="data" class="display" style="width:100%">
-                <thead>
-                    <tr>
-                        <th>Nama Lengkap</th>
-                        <th>Jenis Kelamin</th>
-                        <th>Tempat dan Tanggal Lahir</th>
-                        <th>Agama</th>
-                        <th>Alamat</th>
-                        <th>Aksi</th>
-                    </tr>
-                </thead>
-                <tbody>
-                <?php
+            <table id="data-regis" class="display" style="width:100%">
+        <thead>
+            <tr>
+                <th>Nama Lengkap</th>
+                <th>Jenis Kelamin</th>
+                <th>Tempat dan Tanggal Lahir</th>
+                <th>Agama</th>
+                <th>Alamat</th>
+                <th>Foto</th>
+            </tr>
+        </thead>
+        <tbody> <?php
                     $filename = "db.txt";
                     $handle = fopen($filename, "rb");
                     $contents = fread($handle, filesize($filename));
                     fclose($handle);
-                    echo $contents;
+                   echo $contents;
                 ?>
-                </tbody>
-            </table>
+        </tbody>
+    </table>
         </div>
+       
         <script src="js/jquery-3.4.1.min.js"></script>
         <script type="text/javascript" src="plugins/datatables/datatables.js"></script>
-       <script>
+        <script>
             $(document).ready(function() {
-                $('#data').DataTable( {
+                $('#data-regis').DataTable( {
                     dom: 'Bfrtip',
                     buttons: [
                         'copy', 'pdf', 'print'
